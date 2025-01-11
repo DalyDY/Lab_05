2cdc162999a7ae17ee69b5a59315fe295e8fc88e
Yut_Borey <rayuthchheng@gmail.com>
2025-01-11 15:32:00 -0800

    update the version 1.1

---
+++ Lab_05/src/Session.java
@@ -0,0 +1,40 @@
+
+/**
+ * Create class name Session.java represents a study session of the day.
+ * @author CHHENG Rayuth
+ * @version 1.1
+ * @update create method call setDuration (int start time, and int endTime)
+ * @modified 01/11/2025
+ */
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Session {
+    private String sessionName;
+    private int startTime;
+    private int endTime;
+
+    public Session(String sessionName, int startTime, int endTime) {
+        this.sessionName = sessionName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+
+    }
+    public void setDuration(int startTime, int endTime) throws IllegalArgumentException {
+        if (startTime >= endTime) {
+            throw new IllegalArgumentException("Start time must be less than end time.");
+        }
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
