91d4594a73bd0777f5f5f2017c59a2689bdfa94f
QUT-code <officialrayuth@gmail.com>
2025-01-11 01:19:17 -0800

    Update Session.java without using setter and getter

--- Lab_05/src/Session.java
+++ Lab_05/src/Session.java
@@ -3,22 +3,16 @@
  * @author CHHENG Rayuth
  * @version 1.1
  * @update create method call setDuration(int startTime, int endTime)
+ * 
  * @modified 01/11/2025
  */
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Session {
     private String sessionName;
     private int startTime;
     private int endTime;
+
+    public Session() {
+    }
 
     public Session(String sessionName, int startTime, int endTime) {
         if (startTime >= endTime) {
@@ -29,13 +23,44 @@ public class Session {
         this.endTime = endTime;
     }
 
-    public void setDuration(int startTime, int endTime) throws IllegalArgumentException {
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        if (startTime >= this.endTime) {
+            throw new IllegalArgumentException("Start time must be less than end time.");
+        }
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        if (this.startTime >= endTime) {
+            throw new IllegalArgumentException("End time must be greater than start time.");
+        }
+        this.endTime = endTime;
+    }
+
+    public void setDuration(int startTime, int endTime) {
         if (startTime >= endTime) {
             throw new IllegalArgumentException("Start time must be less than end time.");
         }
         this.startTime = startTime;
         this.endTime = endTime;
     }
+
     @Override
     public String toString() {
         return "Session Name: " + sessionName + "\n" +
