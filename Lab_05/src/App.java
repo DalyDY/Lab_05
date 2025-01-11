1bcb02b511f2acce3d4023e434a151a121c69644
Yut_Borey <rayuthchheng@gmail.com>
2025-01-11 15:32:45 -0800

    update the version 1.1

--- Lab_05/src/App.java
+++ Lab_05/src/App.java
@@ -3,5 +3,7 @@ public class App {
     public static void main(String[] args) {
         Subject subject = new Subject("Mathematics", 101, 3);
         System.out.println(subject);
+        Session session = new Session("Java Workshop", 10, 12);
+        System.out.println(session);
     }
 }
